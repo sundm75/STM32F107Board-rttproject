f487a34af7cf53012cf2fcf14b6d3b4940953ede
sundm75 <sundm75@163.com>
2016-05-16 08:41:53 +0800

    添加多路ADC的相关文件和测试代码

--- RTTexamples/examples/5.ADC转换/applications/adctest.c
+++ RTTexamples/examples/5.ADC转换/applications/adctest.c
@@ -2,8 +2,9 @@
 FileName: adctest.c 
 Author:  孙冬梅       Version :  1.0        Date: 2014.07.30
 Description:    adc.C ADC_Temperature.c 驱动测试程序 
-              1.测量ADC值，并打印
+              1.测量单路ADC值，并打印
               2.测量CPU温度值，并打印 
+              3.测量3路ADC值，并打印 ，其中第一路是单路ADC的值
 Version:         1.0 
 History:         
       <author>  <time>   <version >   <desc> 
@@ -40,4 +41,5 @@ void test_adc (void)
   FINSH_FUNCTION_EXPORT(test_adc, startup adc convert e.g. test_adc());
   FINSH_FUNCTION_EXPORT(getadcvalue, startup adc convert e.g. getadcvalue());
   FINSH_FUNCTION_EXPORT(gettemperature, startup temperature convert e.g. gettemperature());
+  FINSH_FUNCTION_EXPORT(getmuladcvalue, startup temperature convert e.g. getmuladcvalue());
 #endif

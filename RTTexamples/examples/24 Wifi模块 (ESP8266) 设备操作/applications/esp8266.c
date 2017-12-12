9b8bc7ee6ffdcdae9ef27c42f5447c3f6b806419
sundm75 <sundm75@163.com>
2017-12-12 08:34:08 +0800

    采用域名方式连接yeelink

--- RTTexamples/examples/24 Wifi模块 (ESP8266) 设备操作/applications/esp8266.c
+++ RTTexamples/examples/24 Wifi模块 (ESP8266) 设备操作/applications/esp8266.c
@@ -52,7 +52,7 @@ Others:   串口接收数据后，检测 关键字{"value":**}来获取 GET方法得到的数据
 #define ESP8266_CIPMUX "AT+CIPMUX=0\x00D\x00A"      // 设置单连接
 #define ESP8266_CIPMODE "AT+CIPMODE=1\x00D\x00A"      // 设置透传模式
 
-#define ESP8266_CIPSTART "AT+CIPSTART=\"TCP\",\"42.96.164.52\",80\x00D\x00A"      // 建立TCP/UDP连接
+#define ESP8266_CIPSTART "AT+CIPSTART=\"TCP\",\"api.yeelink.net\",80\x00D\x00A"      // 建立TCP/UDP连接
 #define ESP8266_CIPSTATUS "AT+CIPSTATUS\x00D\x00A"      // 获得TCP/UDP连接状态
 #define ESP8266_CIPSEND "AT+CIPSEND="      // 发送数据
 
